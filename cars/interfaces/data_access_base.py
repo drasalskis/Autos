b45596b264d65d127f45ef3eff7e4d24f384f14a
DoMuX <domuxas@gmail.com>
2025-06-11 15:24:25 +0300

    Uzdetos abstractmethod deklaracijos

--- cars/interfaces/data_access_base.py
+++ cars/interfaces/data_access_base.py
@@ -1,13 +1,12 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from cars.models import NewCar, CarFilter
 
 class IDataAccess(ABC):
+    @abstractmethod
     def insert_car(self, new_car : NewCar):
         pass
-
+    
+    @abstractmethod
     def get_data(self, car_filter: CarFilter):
         pass
-
-    def get_data1(self, car_filter: CarFilter):
-        pass    
